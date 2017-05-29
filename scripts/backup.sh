e52e9e72cedce512db267aceb27b1cbda4a2dcb2
Toby Merz <realtiaz@gmail.com>
2017-05-29 13:42:49 +0200

    CHANGE: from absolute to relative path

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/sbin/lvcreate --snapshot --name root_`date +%F` --size 30G ubuntuserver-vg/root /dev/sda1
+lvcreate --snapshot --name root_`date +%F` --size 30G ubuntuserver-vg/root /dev/sda1
