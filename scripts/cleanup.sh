254a103dfc63920d1388d7af8e1b93e0f1146b3e
Toby Merz <realtiaz@gmail.com>
2017-06-05 09:48:12 +0200

    ADD: cleanup by age

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-SNAPSHOTS=$(lvs | awk '/root_/ {print}') # get snapshots of root partition
-SNAPSHOT_NAMES=( $(printf '%s' "${SNAPSHOTS[@]}" | awk '/root_/ {print $1}') )
+#
+# CONFIG
+#
+
+MAX_SIZE=250 # in gigabytes
+MAX_AGE=7 # in days
 
 #
 # CLEANUP BY SIZE
 #
-MAX_SIZE=50 # in gigabytes
+
+SNAPSHOTS=$(lvs | awk '/root_/ {print}') # get snapshots of root partition
+SNAPSHOT_NAMES=( $(printf '%s' "${SNAPSHOTS[@]}" | awk '/root_/ {print $1}') )
 SNAPSHOTS_SIZE=$(printf '%s\n' "${SNAPSHOTS[@]}" | awk '/root_/ {size = size + substr($4, 0, 5)} END {print size}')
 
+echo "Checking maximum size ..."
 if (( $(echo "$SNAPSHOTS_SIZE > $MAX_SIZE" | bc -l) )); then
 	echo "Removing snapshot... ${SNAPSHOT_NAMES[0]}"
 	lvremove -f ubuntuserver-vg/${SNAPSHOT_NAMES[0]}
@@ -19,11 +26,20 @@ fi
 # CLEANUP BY AGE
 #
 
-
-datediff () {
-	d1=$(date -d "$1" +%s)
-	d2=$(date -d "$2" +%s)
-	echo $(( (d1 - d2) / 86400 )) days
-}
-
+SNAPSHOTS=$(lvs | awk '/root_/ {print}') # get snapshots of root partition
+SNAPSHOT_NAMES=( $(printf '%s' "${SNAPSHOTS[@]}" | awk '/root_/ {print $1}') )
 SNAPSHOT_DATES=$(printf '%s\n' "${SNAPSHOTS[@]}" | grep -Po '(?<=root_)([0-9]{4}-[0-9]{2}-[0-9]{2})')
+
+echo "Checking maximum age ..."
+for SNAPSHOT_DATE in $SNAPSHOT_DATES; do
+	TODAY_TIMESTAMP=$(date +%s)
+	SNAPSHOT_TIMESTAMP=$(date -d $SNAPSHOT_DATE +%s)
+	SNAPSHOT_AGE_TIMESTAMP=$(echo "$TODAY_TIMESTAMP - $SNAPSHOT_TIMESTAMP" | bc)
+	SNAPSHOT_AGE_DAYS=$(echo "$SNAPSHOT_AGE_TIMESTAMP / 86400" | bc)
+
+	if (( $(echo "$SNAPSHOT_AGE_DAYS > $MAX_AGE" | bc -l) )); then
+		SNAPSHOT_NAME=$(echo "root_$SNAPSHOT_DATE")
+		echo "Removing snapshot... $SNAPSHOT_NAME"
+		lvremove -f ubuntuserver-vg/${SNAPSHOT_NAME}
+	fi
+done
